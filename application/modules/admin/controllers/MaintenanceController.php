6005c89ce897ec316cd71651c4b1730a81956fa3
pagliaccio <mangheriniroberto@alice.it>
2013-08-30 15:59:12 +0200

    add translate and bugfix

--- application/modules/admin/controllers/MaintenanceController.php
+++ application/modules/admin/controllers/MaintenanceController.php
@@ -10,8 +10,8 @@ class Admin_MaintenanceController extends Zend_Controller_Action
     {
         if (Zend_Registry::isRegistered("param"))
             $this->param = Zend_Registry::get("param");
-        $this->_helper->layout()->nav = array(
-        'log' => array('url' => "admin/maintenance/log"));
+        $this->view->myMenu()
+        ->add('Log','admin','maintenance','log',10,null,null,null,$iconSize,false);
     }
     public function indexAction ()
     {
